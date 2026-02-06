df62e6c9beb5f4ff77486f140f78366b227335d7
Etsoa <fetraniaina26@gmail.com>
2026-02-06 16:15:27 +0300

    reservations
    implementations de la creation et liste de reservation et exposition de l'API pour le frontoffice

---
+++ src/main/java/java/com/backoffice/controller/HotelController.java
@@ -0,0 +1,10 @@
+package java.com.backoffice.controller;
+
+import itu.framework.annotations.MyController;
+import itu.framework.annotations.MyURL;
+import itu.framework.model.ModelView;
+
+@MyController(value = "Hotel")
+public class HotelController {
+
+}
---
+++ src/main/java/java/com/backoffice/models/Reservation.java
@@ -0,0 +1,83 @@
+package java.com.backoffice.models;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reservation")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_reservation")
+    private Integer idReservation;
+
+    @Column(name = "id_client", nullable = false, length = 4)
+    private String idClient;
+
+    @Column(name = "nb_passager", nullable = false)
+    private Integer nbPassager;
+
+    @Column(name = "date_heure_arrivee", nullable = false)
+    private Timestamp dateHeureArrivee;
+
+    @Column(name = "id_hotel", nullable = false)
+    private Integer idHotel;
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer idReservation, String idClient, Integer nbPassager, Timestamp dateHeureArrivee, Integer idHotel) {
+        this.idReservation = idReservation;
+        this.idClient = idClient;
+        this.nbPassager = nbPassager;
+        this.dateHeureArrivee = dateHeureArrivee;
+        this.idHotel = idHotel;
+    }
+
+    public Integer getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(Integer idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public String getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(String idClient) {
+        this.idClient = idClient;
+    }
+
+    public Integer getNbPassager() {
+        return nbPassager;
+    }
+
+    public void setNbPassager(Integer nbPassager) {
+        this.nbPassager = nbPassager;
+    }
+
+    public Timestamp getDateHeureArrivee() {
+        return dateHeureArrivee;
+    }
+
+    public void setDateHeureArrivee(Timestamp dateHeureArrivee) {
+        this.dateHeureArrivee = dateHeureArrivee;
+    }
+
+    public Integer getIdHotel() {
+        return idHotel;
+    }
+
+    public void setIdHotel(Integer idHotel) {
+        this.idHotel = idHotel;
+    }
+}
